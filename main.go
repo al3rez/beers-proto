4145e5c385b27f6103ec7bea2465be9256619c84
Alireza Bashiri <azbshiri@gmail.com>
2019-10-21 01:56:32 +0700

    fix(health)!: fix health checking package name

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 //go:generate go get -u github.com/golang/protobuf/protoc-gen-go
 //go:generate protoc pkg/pb/beers.proto --go_out=plugins=grpc:.
-//go:generate protoc pkg/health/pb/health.proto --go_out=plugins=grpc:.
+//go:generate protoc pkg/pb/grpc_health_v1/health.proto --go_out=plugins=grpc:.
 package main
 
 func main() {}
